661a54960e73c49c61f841ba755ac57d08f6b6fe
Nick Gabe <nicolasgabrielctt@gmail.com>
2023-06-25 18:44:54 -0300

    takes users browser theme into account when starting website

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Sobre from '@/components/Sobre';
 import Projetos from '@/components/Projetos';
 import Footer from '@/components/Footer';
 import { BotaoTema } from '@/components/BotaTema';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export const metadata = {
   title: 'Foolan D\'tal',
@@ -16,13 +16,16 @@ export const metadata = {
 }
 
 export default function Home() {
-  const [tema, setTema] = useState(localStorage.tema || 'light');
+  const temaDoNavegador = useMemo(() =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light'
+  , []);
+  const [tema, setTema] = useState(localStorage.tema || temaDoNavegador);
 
   return (
     <div className={tema}>
       <BotaoTema temaHooks={[tema, setTema]} />
       <div className='dark:bg-dark-gray min-h-screen transition-all'>
-        <div className='max-w-xl mx-auto pt-10 p-6'>
+        <div className='max-w-xl mx-auto pt-16 p-6'>
           <main>
             <Cabecalho />
             <hr className='border-gray-200 dark:border-gray-600' />
