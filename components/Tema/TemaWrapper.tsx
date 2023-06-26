9017d8df04ebc16a462ee7b248603e1eb28497e7
Nícolas Gabriel <nicolasgabrielctt@gmail.com>
2023-06-26 00:13:14 -0300

    applies post mount solution, but starts with darkmode instead (#2)

--- components/Tema/TemaWrapper.tsx
+++ components/Tema/TemaWrapper.tsx
@@ -3,8 +3,12 @@ import { PropsWithChildren, useEffect, useState } from "react";
 import { BotaoTema, Temas } from "../BotaTema";
 
 export const TemaWrapper = (props: PropsWithChildren) => {
-  const temaDoNavegador = window?.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
-  const [tema, setTema] = useState<Temas>(localStorage?.tema || temaDoNavegador);
+  const [tema, setTema] = useState<Temas>('dark');
+
+  useEffect(() => {
+    const temaDoNavegador = window?.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+    setTema(localStorage?.tema || temaDoNavegador);
+  }, [])
 
   return (
     <div className={tema}>
