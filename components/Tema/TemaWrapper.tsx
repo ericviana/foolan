1645087bf002e7e4b5de87a80d59b731df832ae9
Nick Gabe <nicolasgabrielctt@gmail.com>
2023-06-25 22:19:02 -0300

    removes post mount changes of theme color

--- components/Tema/TemaWrapper.tsx
+++ components/Tema/TemaWrapper.tsx
@@ -3,12 +3,8 @@ import { PropsWithChildren, useEffect, useState } from "react";
 import { BotaoTema, Temas } from "../BotaTema";
 
 export const TemaWrapper = (props: PropsWithChildren) => {
-  const [tema, setTema] = useState<Temas>('light');
-
-  useEffect(() => {
-    const temaDoNavegador = window?.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
-    setTema(localStorage?.tema || temaDoNavegador);
-  }, [])
+  const temaDoNavegador = window?.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+  const [tema, setTema] = useState<Temas>(localStorage?.tema || temaDoNavegador);
 
   return (
     <div className={tema}>
